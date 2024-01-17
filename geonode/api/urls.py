bb5abd0ec250551d5eb3d1d7bcac07d019c61a44
Niji Saeba Media Noche <mathereall@gmail.com>
2024-01-17 17:25:10 -0600

    anvcc

--- geonode/api/urls.py
+++ geonode/api/urls.py
@@ -47,6 +47,7 @@ api.register(resourcebase_resources.ResourceBaseResource())
 router = routers.DynamicRouter()
 
 urlpatterns = [
+
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
